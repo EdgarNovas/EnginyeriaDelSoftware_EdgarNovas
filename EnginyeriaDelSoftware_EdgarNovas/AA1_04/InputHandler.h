1c8231cdb2ea893cb244709362191db0eaccebdf
Edgar Novas Paniagua <edgar.novas@enti.cat>
2024-10-14 18:13:28 +0200

    AA1_04 Created

---
+++ EnginyeriaDelSoftware_EdgarNovas/AA1_04/InputHandler.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+class InputHandler {
+private:
+	//std::vector<Controller*> controllers;
+public:
+
+	//void HandleEvent(const Event* event);
+	//void AddController(Controller* controller);
+
+};
