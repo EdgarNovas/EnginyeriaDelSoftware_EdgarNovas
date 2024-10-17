6157c27e713583933ac6504e7473803b57d2be66
Edgar Novas Paniagua <edgar.novas@enti.cat>
2024-10-17 17:58:26 +0200

    AA1_05

---
+++ EnginyeriaDelSoftware_EdgarNovas/AA1_05/DIP/LoadXMLConfigurationService.cpp
@@ -0,0 +1,13 @@
+#include "LoadXMLConfigurationService.h"
+#include <cstdio>
+namespace DIP
+{
+	DIP_Configuration LoadXMLConfigurationService::LoadConfiguration()
+	{
+		// Suponemos que carga el XML
+		std::printf("Cunfiguración cargada desde XML\n");
+		return DIP_Configuration();
+	}
+}
+
+
